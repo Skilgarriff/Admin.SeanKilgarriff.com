3ce7f456f464ba6f914f3478f683ac4bef9fb950
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-11-14 20:36:46 -0600

    add the home package with currently under construction

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react';
+
+
+import styles from './Home.css';
+
+const Home = () =>
+   (
+     <div className={styles.container}>
+       Currently under construction!
+     </div>
+    );
+
+export default Home;
